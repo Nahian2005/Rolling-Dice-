fb793f4495c551a6aac8a659e63703bd3a06648f
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-22 01:46:11 +0600

    Update dr.js

--- dr.js
+++ dr.js
@@ -1,20 +1,26 @@
 
 
-function mydice(){
+var img=["img/2.png", "img/3.png", "img/4.png","img/5.png","img/6.png"]
+var imgTag = document.getElementById("dr");
+var count=0;
 
+function sr(){
+    count++;
    
+if(count>=img.length){
+    count=0;
+    
 
- img= new Array() ;
+  imgTag.src=img[count];  
 
-img[0]= "img/1.png";
-img[1]= "img/2.png";
-img[2]= "img/3.png";
-img[3]= "img/4.png";
-img[4]= "img/5.png";
-img[5]= "img/6.png";
 
-var num= Math.floor(Math.random()*img.length);
-return document.getElementById("result").innerHTML = '<img src="'+img[num]+'" />'; 
+}
+else{
+    imgTag.src=img[count];
+}
+
+
+
 
 
 }
