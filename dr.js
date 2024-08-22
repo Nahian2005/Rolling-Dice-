75791f8bf059893fbe74e86a432e3941241b9464
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-22 15:45:31 +0600

    Update dr.js

--- dr.js
+++ dr.js
@@ -1,23 +1,28 @@
-
-var img=["img/3.png", "img/2.png", "img/5.png","img/6.png","img/4.png"]
+var img=["img/2.png", "img/4.png","img/4.png","img/5.png", "img/6.png", "img/1.png"]
 var imgTag = document.getElementById("dr");
-var count=0;
+
+var count= 0;
+
 
 function sr(){
-    count++;
-   
-if(count>=img.length){
-    count=0;
-    
 
-  imgTag.src=img[count];  
+count++;
+    var num= Math.floor(Math.random()*img.length);
+
+    if(count>=num){
+
+        count=0;
+
+        imgTag.src=img[num]
 
 
-}
-else{
-    imgTag.src=img[count];
-}
 
+    }
+
+    else{
+
+        imgTag.src=img[num];
+    }
 
 
 
